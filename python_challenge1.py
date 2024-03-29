bf0e2698201fdc38faaadc06336412501326fb2f
jackievo3 <163084245+jackievo3@users.noreply.github.com>
2024-03-28 23:56:50 -0400

    Update python_challenge1.py

--- python_challenge1.py
+++ python_challenge1.py
@@ -121,33 +121,45 @@ while place_order:
             menu_selection = input("choose a menu item #")
 
             # 3. Check if the customer typed a number
+            if "menu_item".isdigt():
 
                 # Convert the menu selection to an integer
-
+                menu_item = int("menu_item")
 
                 # 4. Check if the menu selection is in the menu items
-
+                if menu_selection in menu_items.keys():
                     # Store the item name as a variable
 
-
+                    menu_items_name = menu_item
                     # Ask the customer for the quantity of the menu item
-
+                    menu_item_quantity = input("what quantity of + menu_items_name" + "would you like" + "purchase?")
 
                     # Check if the quantity is a number, default to 1 if not
-
-
+                    if "quantity".isdigit():
+                        quantity = int("quantity")
+                    else:
+                        quantity = 1
                     # Add the item name, price, and quantity to the order list
-
+                    order_list.append(
+                        {
+                         "item name": "item_name",
+                         "Price":menu_items[menu_selection]["Price"],
+                         "Quantity": "Quantity"
+                         }
+                    )
 
                     # Tell the customer that their input isn't valid
-
+                else:
+                    print("Menu item imput is incorrect")
 
                 # Tell the customer they didn't select a menu option
 
         else:
+            print("You didnt select a menu option")
             # Tell the customer they didn't select a menu option
             print(f"{menu_category} was not a menu option.")
     else:
+        print("You didnt select a menu option")
         # Tell the customer they didn't select a number
         print("You didn't select a number.")
 
@@ -156,21 +168,21 @@ while place_order:
         keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
 
         # 5. Check the customer's input
+        match keep_ordering.lower():
+            # Customer chose yes
+            case 'y':
+                # Keep ordering
+                place_order = False
 
                 # Keep ordering
-
+                case: 'n'
+                place_order = True
+                
+                print("Thank you for your order.")
                 # Exit the keep ordering question loop
-
-                # Complete the order
-
-                # Since the customer decided to stop ordering, thank them for
-                # their order
-
-                # Exit the keep ordering question loop
-
-
+                break
                 # Tell the customer to try again
-
+print("Please, try again. 'yes' or 'no'. Thank you!")
 
 # Print out the customer's order
 print("This is what we are preparing for you.\n")
@@ -184,17 +196,27 @@ print("--------------------------|--------|----------")
 # 6. Loop through the items in the customer's order
 
     # 7. Store the dictionary items as variables
-
+item_name = ["item_name"]
+price = ["price"]
+quantity = ["quantity"]
 
     # 8. Calculate the number of spaces for formatted printing
-
+num_item_spaces = 26 - len(item_name)
+num_price_spaces = 6 - len(str(price))
+num_quantity_spaces = 10 - len(str(quantity))
+    
 
     # 9. Create space strings
-
+item_spaces = " " * num_item_spaces
+price_spaces = " " * num_price_spaces 
+quantity_spaces = " " * num_quantity_spaces
 
     # 10. Print the item name, price, and quantity
-
+print(item_name,item_spaces,f"${price}{price_spaces} |" + f"{quantity}{quantity_spaces}")
 
 # 11. Calculate the cost of the order using list comprehension
 # Multiply the price by quantity for each item in the order list, then sum()
 # and print the prices.
+
+total_price = sum([item["Price"]*item ["Quantity"]for item in ["order"]])
+print("total prices","total_prices")
